8c9a8df8c63a7df161b67fa54ba433828a489439
Arjan Egges <arjan@quarterfall.com>
2022-06-01 12:26:16 +0200

    Added .gitignore file. Fixed before version of processor.py not containing API key check.

--- before/pay/processor.py
+++ before/pay/processor.py
@@ -5,9 +5,14 @@ class PaymentProcessor:
     def __init__(self, api_key: str) -> None:
         self.api_key = api_key
 
+    def _check_api_key(self) -> bool:
+        return self.api_key == "6cfb67f3-6281-4031-b893-ea85db0dce20"
+
     def charge(self, card: str, month: int, year: int, amount: int) -> None:
         if not self.validate_card(card, month, year):
             raise ValueError("Invalid card")
+        if not self._check_api_key():
+            raise ValueError("Invalid API key")
         print(f"Charging card number {card} for ${amount/100:.2f}")
 
     def validate_card(self, card: str, month: int, year: int) -> bool:
